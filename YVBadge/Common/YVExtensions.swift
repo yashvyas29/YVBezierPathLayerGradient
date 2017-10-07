555feb140be60e74e717a19b2f01cbdf9963c06e
Yash Vyas <yash.vyas.29@gmail.com>
2017-10-07 14:36:16 +0530

    Added string extension to check if a string is url or an image url.

--- YVBadge/Common/YVExtensions.swift
+++ YVBadge/Common/YVExtensions.swift
@@ -82,3 +82,33 @@ extension UIView {
         }
     }
 }
+
+extension String {
+    
+    public func isImage() -> Bool {
+        // Add here your image formats.
+        let imageFormats = ["jpg", "jpeg", "png", "gif"]
+        
+        if let ext = self.getExtension()?.localizedLowercase {
+            return imageFormats.contains(ext)
+        }
+        
+        return false
+    }
+    
+    public func getExtension() -> String? {
+        let ext = (self as NSString).pathExtension
+        
+        if ext.isEmpty {
+            return nil
+        }
+        
+        return ext
+    }
+    
+    public func isURL() -> Bool {
+        let url = URL(string: self)
+        return url != nil && url!.scheme != nil && url!.host != nil
+    }
+    
+}
